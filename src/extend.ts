d978708a19cf342c2975c0a5ed5205b24243b39b
samchungy <samchungy@gmail.com>
2025-07-13 16:26:20 +1000

    add simple tests

--- src/extend.ts
+++ src/extend.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-empty-object-type */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { expect } from 'vitest';
 
 import { allCustomMatcher, type CustomMatcher } from './matcher.js';
@@ -5,8 +7,6 @@ import { allCustomMatcher, type CustomMatcher } from './matcher.js';
 expect.extend(allCustomMatcher);
 
 declare module 'vitest' {
-  // eslint-disable-next-line @typescript-eslint/no-empty-object-type, @typescript-eslint/no-explicit-any
-  interface Assertion<T = any> extends CustomMatcher<T> { }
-  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-  interface AsymmetricMatchersContaining extends CustomMatcher { }
+  interface Assertion<T = any> extends CustomMatcher<T> {}
+  interface AsymmetricMatchersContaining extends CustomMatcher {}
 }
